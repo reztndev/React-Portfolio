5937f64ad109dd71132ccde71d4e3e70223068d2
Mehrdad Rezvany <mrezvany@outlook.com>
2024-09-07 16:06:04 -0700

    Moved static files (e.g. images) to public folder, as well as index.html, to make the app netlify deployable

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -5,42 +5,42 @@ function Portfolio() {
         
         {
             title: 'Kanban Board',
-            image: '../../Assets/kanban-board.png',
+            image: '/images/kanban-board.png',
             technology: 'jQuery',
             deployedApp: 'https://reztndev.github.io/Task-Board/',
             repo: 'https://github.com/reztndev/Task-Board'
         },
         {
             title: 'Song Search',
-            image: '../../Assets/song-search.png',
+            image: '/images/song-search.png',
             technology: 'JavaScript, 3rd Party APIs',
             deployedApp: 'https://koliandrik.github.io/Song-Search/',
             repo: 'https://github.com/koliandrik/Song-Search'
         },
         {
             title: 'Note Taker',
-            image: '../../Assets/note-taker.png',
+            image: '/images/note-taker.png',
             technology: 'REST, Express',
             deployedApp: 'https://note-taker-6pyz.onrender.com',
             repo: 'https://github.com/reztndev/Note-Taker'
         },
         {
             title: 'Personal Blog',
-            image: '../../Assets/personal-blog.png',
+            image: '/images/personal-blog.png',
             technology: 'Web APIs',
             deployedApp: 'https://reztndev.github.io/Web-APIs-Challenge/',
             repo: 'https://github.com/reztndev/Web-APIs-Challenge'
         },
         {
             title: 'Employee Payroll Tracker',
-            image: '../../Assets/payroll-tracker.png',
+            image: '/images/payroll-tracker.png',
             technology: 'JavaScript',
             deployedApp: 'https://reztndev.github.io/JavaScript-Challenge/',
             repo: 'https://github.com/reztndev/JavaScript-Challenge'
         },
         {
             title: 'Profile - Variation',
-            image: '../../Assets/profile-1.png',
+            image: '/images/profile-1.png',
             technology: 'HTML, CSS, JavaScript',
             deployedApp: 'https://reztndev.github.io/Advanced-CSS-Portfolio/',
             repo: 'https://github.com/reztndev/Advanced-CSS-Portfolio'
